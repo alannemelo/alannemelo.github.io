7f626ea7c2e4cc934757f88f725850bd5189f944
leolucena22 <leonardolucena44@gmail.com>
2023-08-10 11:02:08 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+let metros = document.querySelector('#qtd-metros')
+
+let resultado = document.querySelector('#result')
+
+let ano = document.querySelector('#ano')
+
+let dataAtual = new Date()
+let anoAtual = dataAtual.getFullYear()
+
+ano.innerHTML = anoAtual
+
+function imprimeResultado(valor) {
+  resultado.innerHTML = `Quantidade🪨= ${valor} `
+}
+
+function limpar() {
+  metros.value = ''
+  resultado.innerHTML = ''
+}
+
+function vinteQuarenta() {
+  let calculo = Number(metros.value) * 12.5
+  calculo <= 0
+    ? (resultado.innerHTML = `Valor Inválido`)
+    : imprimeResultado(calculo.toFixed(2))
+}
+
+function trinta() {
+  let calculo = Number(metros.value) * 11.11
+  calculo <= 0
+    ? (resultado.innerHTML = `Valor Inválido`)
+    : imprimeResultado(calculo.toFixed(2))
+}
+
+function quarenta() {
+  let calculo = Number(metros.value) * 6.25
+  calculo <= 0
+    ? (resultado.innerHTML = `Valor Inválido`)
+    : imprimeResultado(calculo.toFixed(2))
+}
+
+function cinquenta() {
+  let calculo = Number(metros.value) * 4.0
+  calculo <= 0
+    ? (resultado.innerHTML = `Valor Inválido`)
+    : imprimeResultado(calculo.toFixed(2))
+}
